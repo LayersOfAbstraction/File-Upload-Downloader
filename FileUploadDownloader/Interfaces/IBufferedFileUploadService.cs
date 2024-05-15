036c1bcb59f788f8b652256a54310fd1e8b7ae8b
LayersOfAbstraction <jnash51@live.com>
2024-05-15 12:21:05 +1000

    Change DownloadFile method handshake to Task<FileDownloadModel?> DownloadFile(string fileName);

--- FileUploadDownloader/Interfaces/IBufferedFileUploadService.cs
+++ FileUploadDownloader/Interfaces/IBufferedFileUploadService.cs
@@ -1,9 +1,11 @@
-﻿namespace FileUploadDownloader.Interfaces
+﻿using FileUploadDownloader.FileViewModels;
+
+namespace FileUploadDownloader.Interfaces
 {
     public interface IBufferedFileUploadService
     {
         Task<bool> UploadFile(IFormFile file);
-        Task<byte[]?> DownloadFile(string fileName);
+        Task<FileDownloadModel?> DownloadFile(string fileName);
         string GetContentType(string contentType);
     }
 }
