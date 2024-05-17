49e7aeb8ea259c21fa3aa0f28b7838800a36e69e
LayersOfAbstraction <jnash51@live.com>
2024-05-17 22:53:57 +1000

    Add GetFileModels method ref

--- FileUploadDownloader/FileInterfaces/IBufferedFileUploadService.cs
+++ FileUploadDownloader/FileInterfaces/IBufferedFileUploadService.cs
@@ -1,11 +1,12 @@
 ﻿using FileUploadDownloader.FileViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FileUploadDownloader.FileInterfaces
 {
     public interface IBufferedFileUploadService
     {
         Task<bool> UploadFile(IFormFile file);
-        Task<FileDownloadModel?> DownloadFile(string fileName);
-        string GetContentType(string contentType);
+        Task<byte[]> DownloadFile(string filename);
+        List<FileModel> GetFileModels();
     }
 }
