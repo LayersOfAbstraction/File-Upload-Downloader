058c8762acd86486677d03e6a5ad6025b1c38d50
LayersOfAbstraction <jnash51@live.com>
2024-05-19 18:07:00 +1000

    Add WebRootPath.

--- FileUploadDownloader/Program.cs
+++ FileUploadDownloader/Program.cs
@@ -1,10 +1,19 @@
 using FileUploadDownloader.FileInterfaces;
 using FileUploadDownloader.Services;
+using Microsoft.Extensions.FileProviders;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+//Inject wwwroot/Uploads path to dependency container via IFileProvider. 
+builder.Services.AddSingleton<IFileProvider>(services =>
+{ 
+    var env = services.GetRequiredService<IWebHostEnvironment>();
+    return new PhysicalFileProvider(
+        Path.Combine(env.WebRootPath, "Uploads"));
+});
 
 //Register this service in the dependency container so that it can be injected into a controller
 builder.Services.AddTransient<IBufferedFileUploadService, BufferedFileUploadLocalService>();
